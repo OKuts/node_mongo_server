79d08c1bf23de0c67a2ca3f694b33b281c9d7a66
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-10-17 19:54:17 +0300

    feat: get all users

--- controller/controller.js
+++ controller/controller.js
@@ -2,6 +2,13 @@ const User = require('../models/User');
 const Role = require('../models/Role');
 const bcrypt = require('bcryptjs');
 const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {secret} = require('../config');
+
+const generateAccessToken = (id, roles) => {
+  const payload = {id, roles};
+  return jwt.sign(payload, secret, {expiresIn: '24h'});
+}
 
 class Controller {
   async registration(req, res) {
@@ -26,7 +33,13 @@ class Controller {
 
   async login(req, res) {
     try {
-
+      const {username, password} = req.body;
+      const user = await User.findOne({username});
+      if (!user) return res.status(400).json({message: `User ${username} not found`});
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) return res.status(400).json({message: `Password not correct`});
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({token})
     } catch (e) {
       console.log(e);
       res.status(400).json({message: 'Registation error'})
@@ -39,7 +52,8 @@ class Controller {
       // const adminRole = new Role({value: 'ADMIN'})
       // await userRole.save();
       // await adminRole.save();
-      res.json('Ok')
+      const users = await User.find();
+      res.json(users)
     } catch (e) {
 
     }
